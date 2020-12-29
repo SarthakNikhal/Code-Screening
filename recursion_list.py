dc407573a3847c368e475ff7c815cdd42c58822c
Sarthak Nikhal <56469758+SarthakNikhal@users.noreply.github.com>
2020-12-30 00:40:15 +0530

    Added docstring and comments

--- recursion_list.py
+++ recursion_list.py
@@ -1,9 +1,10 @@
+""" Function to recursively print every 3rd element from i"""
 def recursion(a_list, i):
-    print(a_list[i])
+    print(a_list[i])                   # Printing the element
     
     if(i + 3 < len(a_list)):
-        recursion(a_list, i + 3)
+        recursion(a_list, i + 3)       # Calling recursively
 
 i = 2
 some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-recursion(some_list, i)
+recursion(some_list, i)                # Initial function call
